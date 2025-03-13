8ee363287b33d4a44b1e96363d77bd393db4c7e5
yuriynefedov <theyuriynefeedov@gmail.com>
2025-03-13 13:04:20 +0200

    Introduce View+ProgressBlurred

---
+++ Sources/HeliconUI/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  HeliconUI
+//
+//  Created by Yuriy Nefedov on 13.03.2025.
+//
+
+import Foundation
---
+++ Sources/HeliconUI/View Modifiers/View+ProgressBlurred.swift
@@ -0,0 +1,56 @@
+//
+//  View+ProgressBlurred.swift
+//  App
+//
+//  Created by Yuriy Nefedov on 28.11.2024.
+//
+
+import SwiftUI
+
+struct ProgressBlurredViewModifier: ViewModifier {
+    let isActive: Bool
+    var showSpinner: Bool = true
+    
+    func body(content: Content) -> some View {
+        if isActive {
+            ZStack {
+                content
+                    .blur(radius: 10)
+                if showSpinner {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                }
+            }
+        } else {
+            content
+        }
+    }
+}
+
+extension View {
+    func progressBlurred(_ isActive: Bool = true, showSpinner: Bool = true) -> some View {
+        self.modifier(ProgressBlurredViewModifier(isActive: isActive))
+    }
+}
+
+fileprivate struct ProgressBlurredExampleView: View {
+    @State private var loading = true
+    
+    var body: some View {
+        Text("ProgressBlurred")
+            .progressBlurred(loading)
+            .onAppear {
+                Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { _ in
+                    DispatchQueue.main.async {
+                        withAnimation {
+                            loading.toggle()
+                        }
+                    }
+                }
+            }
+    }
+}
+
+#Preview {
+    ProgressBlurredExampleView()
+}
