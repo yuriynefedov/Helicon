fa03f29672f4b10c0f9477ce322cab069e2ba5e4
yuriynefedov <theyuriynefeedov@gmail.com>
2025-03-13 12:54:37 +0200

    Initial commit

---
+++ Sources/HeliconUI/HeliconUI.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct HeliconUI {
+    public static func sayHello() -> String {
+        return "Hello from MyPackage!"
+    }
+}
