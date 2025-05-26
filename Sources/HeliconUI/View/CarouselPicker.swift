0f36296012706856fcad7a44ed03026b492d2fa5
yuriynefedov <theyuriynefeedov@gmail.com>
2025-05-26 20:39:07 +0300

    CarouselPicker access modifiers & semantic improvements

--- Sources/HeliconUI/View/CarouselPicker.swift
+++ Sources/HeliconUI/View/CarouselPicker.swift
@@ -7,19 +7,20 @@
 
 import SwiftUI
 
-typealias CarouselItem = Hashable & Identifiable
+public typealias CarouselItem = Hashable & Identifiable
 
-struct CarouselView<Content: View, Item: CarouselItem>: View {
+public struct CarouselPicker<Content: View, Item: CarouselItem>: View {
     @Binding var items: [Item]
-    @Binding var selectedItem: Item
+    @Binding var selection: Item
     
-    var config: Config
+    var configuration: Configuration
     
     @ViewBuilder let content: (Item) -> Content
     
     @State private var contentSize: CGSize = .zero
     @State private var scrollPosition: Item.ID?
-    var body: some View {
+    
+    public var body: some View {
         Color.clear
             .readSize { size in
                 contentSize = size
@@ -27,43 +28,43 @@ struct CarouselView<Content: View, Item: CarouselItem>: View {
             .overlay {
                 ScrollViewReader { proxy in
                     ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: config.spacing) {
+                        HStack(spacing: configuration.spacing) {
                             ForEach(items) { item in
                                 Rectangle()
                                     .fill(Color.clear)
-                                    .frame(width: max(contentSize.width - (config.contentInset.left + config.contentInset.right), 0), height: max(contentSize.height, 0))
+                                    .frame(width: max(contentSize.width - (configuration.contentInset.left + configuration.contentInset.right), 0), height: max(contentSize.height, 0))
                                     .overlay(
                                         content(item)
                                             .id(item.id)
                                     )
                                     .scrollTransition { content, phase in
                                         content
-                                            .opacity(phase.isIdentity ? 1 : config.opacityEffect)
-                                            .scaleEffect(y: phase.isIdentity ? 1 : config.scaleEffect)
+                                            .opacity(phase.isIdentity ? 1 : configuration.opacityEffect)
+                                            .scaleEffect(y: phase.isIdentity ? 1 : configuration.scaleEffect)
                                     }
                             }
                         }
                         .scrollTargetLayout()
                     }
-                    .contentMargins(.leading, config.contentInset.left, for: .scrollContent)
-                    .contentMargins(.trailing, config.contentInset.right, for: .scrollContent)
+                    .contentMargins(.leading, configuration.contentInset.left, for: .scrollContent)
+                    .contentMargins(.trailing, configuration.contentInset.right, for: .scrollContent)
                     .scrollTargetBehavior(.viewAligned)
                     .scrollPosition(id: $scrollPosition)
                     .onChange(of: scrollPosition) { _, newValue in
                         if let id = newValue, let foundItem = items.first(where: { $0.id == id }) {
-                            selectedItem = foundItem
+                            selection = foundItem
                         }
                     }
                     .onAppear {
                         withAnimation(.easeInOut) {
-                            proxy.scrollTo(selectedItem.id, anchor: .center)
+                            proxy.scrollTo(selection.id, anchor: .center)
                         }
                     }
                 }
             }
     }
     
-    struct Config {
+    public struct Configuration {
         var opacityEffect: CGFloat = 1.0
         var scaleEffect: CGFloat = .zero
         
@@ -73,13 +74,13 @@ struct CarouselView<Content: View, Item: CarouselItem>: View {
 }
 
 
-struct CarouselTestView: View {
+fileprivate struct CarouselTestView: View {
     @State var items: [Mock] = Mock.list
     @State var selectedItem: Mock = Mock.list[0]
     var body: some View {
-        CarouselView(items: $items,
+        CarouselPicker(items: $items,
                      selectedItem: $selectedItem,
-                     config: .init(
+                     configuration: .init(
                         opacityEffect: 0.3,
                         scaleEffect: 0.7,
                         spacing: .zero,
@@ -99,7 +100,7 @@ struct CarouselTestView: View {
     }
 }
 
-struct SizePreferenceKey: @preconcurrency PreferenceKey {
+fileprivate struct SizePreferenceKey: @preconcurrency PreferenceKey {
     @MainActor static var defaultValue: CGSize = .zero
     static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
         value = nextValue()
@@ -107,7 +108,7 @@ struct SizePreferenceKey: @preconcurrency PreferenceKey {
 }
 
 
-extension View {
+fileprivate extension View {
     func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
         overlay(
             GeometryReader { proxy in
