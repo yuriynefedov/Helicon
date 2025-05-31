c30c79d904da14d0f4f27e2b21cda226d68574f2
yuriynefedov <theyuriynefeedov@gmail.com>
2025-05-31 17:27:11 +0300

    Introduce DeviceWrapped & Asset Catalog

---
+++ Sources/HeliconUI/View Modifier/View+DeviceWrapped.swift
@@ -0,0 +1,59 @@
+//
+//  DeviceFrame.swift
+//  Helicon Widgets
+//
+//  Created by Yuriy Nefedov on 23.05.2025.
+//
+
+import SwiftUI
+
+struct DeviceFrame<Content: View>: View {
+    let content: () -> Content
+    
+    var body: some View {
+        GeometryReader { proxy in
+            
+            let frameAspectRatio: CGFloat = 0.483
+            let inferredFrameWidth: CGFloat = min(proxy.size.width, proxy.size.height * frameAspectRatio)
+            
+            let inferredCornerRadius: CGFloat = inferredFrameWidth / 7
+            let inferedBezzleWidth: CGFloat = inferredFrameWidth / 26
+            let inferedBezzleHeight: CGFloat = inferredFrameWidth / 30
+            
+            ZStack {
+                deviceFrame
+                    .background(
+                        content()
+                            .clipShape(RoundedRectangle(cornerRadius: inferredCornerRadius))
+                            .padding(.horizontal, inferedBezzleWidth)
+                            .padding(.vertical, inferedBezzleHeight)
+                    )
+                    
+            }
+            .position(
+                x: proxy.size.width / 2,
+                y: proxy.size.height / 2
+            )
+        }
+    }
+    
+    private var deviceFrame: some View {
+        Image("Frame.iPhone16Pro.DesertTitanium")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+public extension View {
+    func deviceWrapped() -> some View {
+        DeviceFrame {
+            self.body
+        }
+    }
+}
+
+#Preview {
+    DeviceFrame {
+        Color.blue.opacity(0.5)
+    }
+}
