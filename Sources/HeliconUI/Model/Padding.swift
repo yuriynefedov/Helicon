b48fa2e02c447455fd44d6333d070560348677a6
yuriynefedov <theyuriynefeedov@gmail.com>
2025-06-13 18:16:31 +0300

    Provide default padding edges value

--- Sources/HeliconUI/Model/Padding.swift
+++ Sources/HeliconUI/Model/Padding.swift
@@ -22,7 +22,7 @@ public enum Padding {
 }
 
 public extension View {
-    func padding(_ edges: Edge.Set, _ padding: Padding) -> some View {
+    func padding(_ edges: Edge.Set = .all, _ padding: Padding) -> some View {
         self.body.padding(edges, padding.value)
     }
 }
